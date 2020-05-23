71a11ac669fc0fc3696a21103b61f548545c6ce5
fabianzielinski <fabian.zielinski@o2.pl>
2020-05-23 22:02:52 +0200

    Add cursor

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
 const txt = 'Id est et excepteur ipsum occaecat irure mollit consectetur cillum qui duis nisi. Culpa nostrud consectetur id Lorem eu reprehenderit aliquip cupidatat officia excepteur culpa laboris pariatur. Deserunt mollit enim aliquip incididunt. Cillum laboris commodo commodo id est fugiat exercitation nisi proident incididunt amet eiusmod duis excepteur. Mollit officia sunt adipisicing non elit do adipisicing dolore. Ex irure voluptate eiusmod culpa excepteur exercitation. Fugiat et occaecat minim quis voluptate deserunt deserunt.'
 
 
 let indexText = 0;
+const time = 70;
 
 const addLeter = () => {
     spnText.textContent += txt[indexText];
@@ -10,5 +12,14 @@ const addLeter = () => {
     if(indexText === txt.length) clearInterval(indexTyping);
 }
 
-const indexTyping = setInterval(addLeter, 50);
+const cursorAnimation = ()=> {
+    console.log("ok");
+    spnCursor.classList.toggle('active');
+}
+
+const indexTyping = setInterval(addLeter, time);
+
+setInterval(cursorAnimation, 400);
+
+
 
